12b0990a2aa559d2833e6c2ce104d2b879cc969e
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-02-27 11:20:41 +0300

    Update entire code

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# Cheating in pogemon card game
+
+## The rules
+
+    Feu vs Eau      | Eau l'emporte
+    Feu vs Plante   | Feu l'emporte
+    Feu vs Glace    | Feu l'emporte
+    
+    Eau vs Plante   | Plante l'emporte
+    Eau vs Sol      | Sol l'emporte
+    
+    Plante vs Poison | Plante l'emporte
+    Plante vs Sol    | Sol l'emporte
+    Plante vs Vol    | Plante l'emporte
+
+[Visit this link for more info about this game](http://example.net/)
+
+> Implementing this game was pretty handy but cool.
+
+# Some help are Welcome
+
+## That's said, I need help to fix some issues and maybe enhance this game
+
+1. Performance issues
+    >The runtime complexity of this game is `horrible`
+    * his is probably caused by the permutation generator function
+
+2. Build a friendly User interface
+    > At this state only programmer, or some user can use this game
+    As on of my friends said when he saw me using the terminal to run some command:
+    " You're a Hacker ?? "
---
+++ test/pogemon.test.js
@@ -0,0 +1,43 @@
+import {expect} from "chai"
+import {pogemon, rules} from "../pogemon.js";
+
+describe("The Pogemon game", () => {
+
+    describe('Rules validation', () => {
+        it('should return the winner card between two different card and swap if nedded', () => {
+            expect(rules('plante', 'poison')).to.be.eq('plante')
+            expect(rules('feu', 'eau')).to.be.eq('eau')
+            expect(rules('eau', 'feu')).to.be.eq('eau')
+        })
+        it('should return n if the macth is null', () => {
+            expect(rules('eau', 'eau')).to.deep.eql('n')
+            expect(rules('plante', 'plante')).to.deep.eql('n')
+        })
+        it('should return n if at least one invalid card was parsed', () => {
+            expect(rules('holla', 'plante')).to.deep.eql('n')
+            expect(rules('plante', 'holla')).to.deep.eql('n')
+            expect(rules('bla', 'okala')).to.deep.eql('n')
+        })
+    })
+
+    describe("main function", () => {
+
+        it('should return -1 if there is no possible card order were sacha win', () => {
+            expect(pogemon([3, "sol sol eau", "eau feu plante"])).to.be.eql(-1)
+            expect(pogemon([3, "feu feu feu", "feu feu feu"])).to.be.eql(-1)
+        })
+        it('should return the winner card in the right order', () => {
+            expect(pogemon([4, "feu plante eau plante", "feu plante eau poison"])).to.be.eql("eau feu plante poison")
+            expect(pogemon([3, "alika akoho feu", "feu eau plante"])).to.be.eql("feu plante eau")
+            expect(pogemon(
+                [
+                    '10',
+                    'poison glace eau eau eau vol plante vol poison plante',
+                    'plante sol plante plante feu sol plante plante plante plante']
+                )
+            ).to.be.eql("plante sol plante plante feu sol plante plante plante plante")
+        })
+
+    })
+
+})
